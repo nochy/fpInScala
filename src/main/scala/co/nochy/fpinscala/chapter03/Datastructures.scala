ceb2ace2da295588bd82404ec2480b9fe1825680
nochy <radius.t.n@gmail.com>
2014-10-24 19:06:39 +0900

    2014/10/24 chapter3

--- src/main/scala/co/nochy/fpinscala/chapter03/Datastructures.scala
+++ src/main/scala/co/nochy/fpinscala/chapter03/Datastructures.scala
@@ -16,4 +16,39 @@ object List {
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
+
+  def tail[A](l: List[A]): List[A] = l match {
+    case (Cons(_, x)) => x
+    case _ => Nil
+  }
+
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case (Cons(x, y)) => Cons(h, y)
+    case _ => Nil
+  }
+
+  // deleteになってた…
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) {
+      l
+    } else if (n == 1) {
+      tail(l)
+    } else {
+      l match {
+        case Cons(x, y) => Cons(x, drop(y, n -1))
+        case _ => Nil
+      }
+    }
+
+  def drop2[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) {
+      l
+    } else {
+      l match {
+        case Cons(_, y) => drop(y, n-1)
+        case _ => Nil
+      }
+    }
+
 }
+
--- src/test/scala/co/nochy/fpinscala/chapter03/DatastructuresSpec.scala
+++ src/test/scala/co/nochy/fpinscala/chapter03/DatastructuresSpec.scala
@@ -12,6 +12,60 @@ class DatastructuresSpec extends Specification {
       val ex3: List[String] = Cons("a", Cons("b", Nil))
       true
     }
+
+    "c" in {
+      val ex1: List[Double] = Cons(1.0, Cons(2.0, Cons(3.0, Nil)))
+      List.product(ex1) must_== 6.0
+    }
+
+    "exercise1" in {
+      val a = List(1,2,3,4,5) match {
+        case Cons(x, Cons(2, Cons(4, _))) => x
+        case Nil => 42
+        case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+        case Cons(h, t) => h + List.sum(t)
+        case _ => 101
+      }
+      a must_== 3
+    }
+  }
+
+  "exercise3.2" should {
+    "tail" in {
+      List.tail(List(1, 2, 3)) must_== List(2, 3)
+    }
+  }
+
+  "exercise3.3" should {
+    "setHead" in {
+      List.setHead(List(1,2,3), 4) must_== List(4,2,3)
+    }
+  }
+
+  "exercise3.4勘違い" should {
+    "drop no 1" in {
+      List.drop(List(1,2,3,4,5), 1) must_== List(2,3,4,5)
+    }
+    "drop no 2" in {
+      List.drop(List(1,2,3,4,5), 2) must_== List(1,3,4,5)
+    }
+    "drop out of no" in {
+      List.drop(List(1,2,3,4,5), 6) must_== List(1,2,3,4,5)
+    }
+  }
+
+  "exercise3.4(answer)" should {
+    "drop no 1" in {
+      List.drop2(List(1,2,3,4,5), 1) must_== List(2,3,4,5)
+    }
+    "drop no 2" in {
+      List.drop2(List(1,2,3,4,5), 2) must_== List(3,4,5)
+    }
+    "drop out of no(何か違う気がする)" in {
+      // こうなると思ってた
+      // List.drop2(List(1,2,3,4,5), 6) must_== Nil
+      List.drop2(List(1,2,3,4,5), 6) must_== List(2,3,4,5)
+    }
   }
 
 }
